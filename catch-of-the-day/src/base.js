379f211e3e0f0f5ead29ed07ea78457dfb9cc4d0
Michael Rivera <mrivera.dev@gmail.com>
2016-12-27 17:09:54 -0500

    Part 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,11 @@
+import Rebase from 're-base';
+
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBA-WDMH3BOh28WxTKMOggYkyXl7tryaJc",
+    authDomain: "catch-of-the-day-mike-rivera.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-mike-rivera.firebaseio.com",
+});
+
+
+export default base;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -12,14 +12,14 @@ class StorePicker extends React.Component {
     console.log("You changed the URL");
     // first grab text from box
     const storeId = this.storeInput.value;
-    console.log(this.storeInput.value);
+    console.log(`Going to ${storeId}`);
     // transition from / to /store/:storeId
-    this.context.router.transitionTo("/store/${storeId}");
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"
         defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
