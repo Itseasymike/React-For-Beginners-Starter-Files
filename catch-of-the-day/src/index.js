84916646bd6b542310c8ee1df9bfc6300b5cc349
Michael Rivera <mrivera.dev@gmail.com>
2016-12-28 23:21:59 -0500

    Part 27

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -11,8 +11,9 @@ import NotFound from './components/NotFound';
 
 
 const Root = () => {
+  const repo = `/${window.location.pathname.split('/')[1]}`;
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={repo}>
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match exactly pattern="/store/:storeId" component={App} />
